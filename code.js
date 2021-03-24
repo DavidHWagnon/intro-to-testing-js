4ef83e27c574d2a25fa5f24fc9b03a75a49f3c68
David Wagnon <david.wagnon02@gmail.com>
2021-03-24 12:23:22 -0500

    Completed Exercise #6

--- code.js
+++ code.js
@@ -1,17 +1,6 @@
 // helloWorld function 
 function helloWorld(input) {
-    if(input==="Alex"){
-        return "Hello, Alex!"
-    }
-    if(input==="Jane"){
-        return "Hello, Jane!"
-    }
-    if(input==="Pat"){
-        return "Hello, Pat!"
-    }
-    else{
-        return "";
-    }
+    return "Hello, " + input + "!";
 }
 
 //addOne
